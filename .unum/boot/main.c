55551aa85bf05580ab1fc384a1beed96dd9b9084
frangrolemund <frangrolemund@users.noreply.github.com>
2024-11-16 10:07:06 -0500

    Refinements.

--- .unum/boot/main.c
+++ .unum/boot/main.c
@@ -1,3 +1,32 @@
+/*---------------------------------------------------------------
+| vi: set noet ts=4 sw=4 fenc=utf-8
+| ---------------------------------------------------------------
+| Copyright 2024 Francis Henry Grolemund III
+|
+| Permission to use, copy, modify, and/or distribute this software for
+| any purpose with or without fee is hereby granted, provided that the
+| above copyright notice and this permission notice appear in all copies.
+|
+| THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+| WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+| WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+| AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+| DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+| PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+| TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+| PERFORMANCE OF THIS SOFTWARE.
+| ---------------------------------------------------------------*/
+
+/*
+ *  The purpose of bootstrapping is to ingest the C compiler environment
+ *  from a platform-specific toolchain (ie. `make`) into the 
+ *  platform-independent `unum` toolchain and have the latter take over further
+ *  build processing.  
+ *
+ *  This program figures out where it is running, encodes fundamental 
+ *  configuration and builds the first `unum` kernel.
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
