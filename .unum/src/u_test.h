97d6e9aaafcedd3a693ccc79e37657ed9c2ddd90
frangrolemund <frangrolemund@users.noreply.github.com>
2024-11-28 09:03:26 -0500

    Unit test structure.

---
+++ .unum/src/u_test.h
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------
+| vi: set noet ts=4 sw=4 fenc=utf-8
+| ---------------------------------------------------------------
+| Copyright 2024 Francis Henry Grolemund III
+|
+| Permission to use, copy, modify, and/or distribute this software for
+| any purpose with or without fee is hereby granted, provided that the
+| above copyright notice and this permission notice appear in all copies.
+|
+| THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+| WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+| WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+| AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+| DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+| PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+| TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+| PERFORMANCE OF THIS SOFTWARE.
+| ---------------------------------------------------------------*/
+
+#ifndef UNUM_TEST_H
+#define UNUM_TEST_H
+
+#include <stdio.h>
+#include "common.h"
+
+#ifndef UNUM_UNIT_TEST
+#error "Unit testing only."
+#endif
+
+#endif /* UNUM_TEST_H */
