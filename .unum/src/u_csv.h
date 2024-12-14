6b304116a1e95cf96cde57d2447c8948327c42cb
frangrolemund <frangrolemund@users.noreply.github.com>
2024-12-14 09:26:26 -0500

    Formatting.

--- .unum/src/u_csv.h
+++ .unum/src/u_csv.h
@@ -38,7 +38,7 @@ typedef struct {
  * UU_csv_new()
  * - create a new in-memory CSV instance to be later freed with UU_csv_delete().
  */
-extern uu_csv_t    *UU_csv_new( unsigned cols );
+extern uu_csv_t     *UU_csv_new( unsigned cols );
 
 
 /*
@@ -46,7 +46,7 @@ extern uu_csv_t    *UU_csv_new( unsigned cols );
  * - open an existing CSV file from the provided path to be later freed with
  *   UU_csv_delete().
  */
-extern uu_csv_t    *UU_csv_open( uu_cstring_t path, uu_error_e *err );
+extern uu_csv_t     *UU_csv_open( uu_cstring_t path, uu_error_e *err );
 
 
 /*
@@ -54,64 +54,64 @@ extern uu_csv_t    *UU_csv_open( uu_cstring_t path, uu_error_e *err );
  * - open an existing CSV file from the provided buffer to be later freed with
  *   UU_csv_delete().
  */
-extern uu_csv_t    *UU_csv_memory( uu_cstring_t buf, uu_error_e *err );
+extern uu_csv_t     *UU_csv_memory( uu_cstring_t buf, uu_error_e *err );
 
 
 /*
  * UU_csv_write()
  * - write the CSV file to the path or NULL to use the same path as input.
  */
-extern int         UU_csv_write( uu_cstring_t path );
+extern int          UU_csv_write( uu_cstring_t path );
 
 
 /*
  * UU_csv_delete()
  * - free resources for CSV instance.
  */
-extern void        UU_csv_delete( uu_csv_t *csv );
+extern void         UU_csv_delete( uu_csv_t *csv );
 
 /*
  * UU_csv_add_row()
  * - add a new row to the table.
  */
-extern int         UU_csv_add_row( uu_csv_t *csv );
+extern int          UU_csv_add_row( uu_csv_t *csv );
 
 
 /*
  * UU_csv_get()
  * - get the value from a specific cell in the CSV file.
  */
-extern const char  *UU_csv_get( uu_csv_t *csv, unsigned row, unsigned col,
-							    uu_error_e *err );
+extern const char   *UU_csv_get( uu_csv_t *csv, unsigned row, unsigned col,
+							     uu_error_e *err );
 
 
 /*
  * UU_csv_set()
  * - assign the value of a specific cell in the CSV file.
  */
-extern int         UU_csv_set( uu_csv_t *csv, unsigned row, unsigned col,
-                               uu_string_t *value );
+extern int          UU_csv_set( uu_csv_t *csv, unsigned row, unsigned col,
+                                uu_string_t *value );
 
 
 /*
  * UU_csv_rows()
  * - get the number of rows in the CSV file.
  */
-extern unsigned    UU_csv_rows( uu_csv_t *csv );
+extern unsigned     UU_csv_rows( uu_csv_t *csv );
 
 
 /*
  * UU_csv_cols()
  * - get the number of columns in the CSV file.
  */
-extern unsigned    UU_csv_cols( uu_csv_t *csv );
+extern unsigned     UU_csv_cols( uu_csv_t *csv );
+
 
 /*
  * UU_csv_file_path()
  * - get the file path associated with the CSV file or NULL if unset.
  */
 extern uu_cstring_t UU_csv_file_path( uu_csv_t *csv );
-
 
 
 #endif /* UNUM_CSV_H */
