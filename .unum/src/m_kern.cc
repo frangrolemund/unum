b849c9fb17e2d49c7b6af917bd6888c0aa0f4709
frangrolemund <frangrolemund@users.noreply.github.com>
2025-02-07 09:33:16 -0500

    Added comment.

--- .unum/src/m_kern.cc
+++ .unum/src/m_kern.cc
@@ -13,6 +13,9 @@
 int un::main(int argc, char **argv) {
 	if (argc > 2 && !std::strcmp(argv[1], "deploy") &&
 	    !std::strcmp(argv[2], "--bootstrap")) {
+	    // following a principle of trust-but-verify, the kernel validates its
+	    // own deployment created by the pre-kernel and only signs off if it
+	    // arrives at a similar result.
 	    std::printf("unum:  unum is bootstrapped.\n");
 	} else {
 		std::printf("unum: TODO\n");
